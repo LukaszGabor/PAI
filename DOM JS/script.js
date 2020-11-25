1b0af862cb16cc4ee47c5c1394677ac0a530fe3b
LukaszGabor <lukaszgabor02@gmail.com>
2020-11-25 12:32:27 +0100

    update

--- DOM JS/script.js
+++ DOM JS/script.js
@@ -1,2 +1,7 @@
 const titleDoc = document.getElementById('titleDoc');
 console.log(titleDoc);
+titleDoc.innerText = 'Hello World';
+
+const wybor3 = document.getElementById('wybor3');
+console.log(wybor3);
+wybor3.checked = true;
